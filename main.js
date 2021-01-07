07c826320540812999ffaf472e6d3f30cb10f00d
GurraT <gustafthomsson@hotmail.com>
2021-01-07 20:18:00 +0000

    work in progress

--- main.js
+++ main.js
@@ -1,31 +1,53 @@
 
+
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+  
+function getData(type, cb) {
+
 //request method //
 var xhr = new XMLHttpRequest();
-var data;
-
-
-function setData(jsonData) {
-    data = jsonData;
-}
-
-//event listner see linkfolder for info about readyState and status//
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-       // console.log(typeof(JSON.parse(this.responseText))); //
-       //console.log(JSON.parse(this.responseText)); //
-       setData(JSON.parse(this.responseText));
-
-    }
-};
+//var data;//
 
 //get info from adress//
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.open("GET", baseURL + type + "/");
 
 //send info data//
 
 xhr.send();
 
-console.log(data);
+
+//function setData(jsonData) {//
+    //data = jsonData; } //
+
+//event listner see linkfolder for info about readyState and status//
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // document.getElementById("data").innerHTML = this.responseText;//
+       // console.log(typeof(JSON.parse(this.responseText))); //
+       //console.log(JSON.parse(this.responseText)); //
+       //setData(JSON.parse(this.responseText));//
+        //(data=JSON.parse(this.responseText); //
+          cb(JSON.parse(this.responseText));
+    }
+}
+};
+
+//run code after 500 millisec //
+
+//setTimeout(function(){//
+//console.log(data);//
+//}, 500);//
+
+//function printDataToConsole(data) {
+  //  console.log(data);
+//}
+
+//getData(printDataToConsole);
+
+function writeToDocument(type){
+    getData(type, function(data){
+    document.getElementById("data").innerHTML=data;
+});
+}
